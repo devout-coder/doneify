bed3573567c5f05ba5fa8e06e81254974a086955
devout-coder <awessssomepro@gmail.com>
2022-12-21 17:48:38 +0530

    built the UI of signup and login page

--- lib/pages/Auth.dart
+++ lib/pages/Auth.dart
@@ -10,6 +10,15 @@ class Auth extends StatefulWidget {
 }
 
 class _AuthState extends State<Auth> {
+  double borderRadius = 10;
+  bool _passwordVisible = false;
+  Color textFieldActiveColor = Color.fromARGB(255, 230, 230, 230);
+  Color textFieldInactiveColor = Colors.grey;
+
+  String username = '';
+  String email = "";
+  String password = "";
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -29,14 +38,15 @@ class _AuthState extends State<Auth> {
                     Color.fromARGB(71, 217, 217, 217)),
                 shape: MaterialStateProperty.all(
                   RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20),
+                    borderRadius: BorderRadius.circular(borderRadius),
                   ),
                 ),
                 backgroundColor: MaterialStateProperty.all(Colors.transparent),
               ),
-              child: Ink(
+              child: Container(
+                height: 50,
                 width: 320,
-                decoration: const BoxDecoration(
+                decoration: BoxDecoration(
                   gradient: LinearGradient(
                     colors: [
                       Color.fromARGB(15, 217, 217, 217),
@@ -45,47 +55,175 @@ class _AuthState extends State<Auth> {
                     begin: Alignment.topLeft,
                     end: Alignment.bottomCenter,
                   ),
-                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                  borderRadius: BorderRadius.all(Radius.circular(borderRadius)),
                 ),
-                child: Container(
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      SizedBox(width: 25),
-                      Image.asset("assets/images/Google.png"),
-                      SizedBox(width: 15),
-                      Text("Continue with Google",
-                          style: const TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.w600,
-                          )),
-                      SizedBox(width: 200, height: 50),
-                    ],
-                  ),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    SizedBox(width: 25),
+                    Image.asset("assets/images/Google.png"),
+                    SizedBox(width: 15),
+                    Text(
+                      "Continue with Google",
+                      style: const TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
           ),
-          SizedBox(height: 30),
+          SizedBox(height: 15),
+          Text(
+            "or",
+            style: TextStyle(color: Colors.white, fontSize: 16),
+          ),
+          SizedBox(height: 15),
+          widget.type == "signup"
+              ? (Column(
+                  children: [
+                    SizedBox(
+                      width: 315,
+                      height: 85,
+                      child: Padding(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                        child: TextField(
+                          style: TextStyle(color: textFieldActiveColor),
+                          decoration: InputDecoration(
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: textFieldInactiveColor, width: 1),
+                              borderRadius: BorderRadius.all(
+                                  Radius.circular(borderRadius)),
+                            ),
+                            hintStyle: TextStyle(color: textFieldInactiveColor),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: textFieldActiveColor, width: 1),
+                              borderRadius: BorderRadius.all(
+                                  Radius.circular(borderRadius)),
+                            ),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                  Radius.circular(borderRadius)),
+                            ),
+                            hintText: "Username",
+                          ),
+                          onChanged: (text) {
+                            username = text;
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
+                ))
+              : (Container()),
+          SizedBox(
+            width: 315,
+            height: 85,
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+              child: TextField(
+                style: TextStyle(color: textFieldActiveColor),
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderSide:
+                        BorderSide(color: textFieldInactiveColor, width: 1),
+                    borderRadius:
+                        BorderRadius.all(Radius.circular(borderRadius)),
+                  ),
+                  hintStyle: TextStyle(color: textFieldInactiveColor),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide:
+                        BorderSide(color: textFieldActiveColor, width: 1),
+                    borderRadius:
+                        BorderRadius.all(Radius.circular(borderRadius)),
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius:
+                        BorderRadius.all(Radius.circular(borderRadius)),
+                  ),
+                  hintText: "Email",
+                ),
+                onChanged: (text) {
+                  email = text;
+                },
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 315,
+            height: 85,
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+              child: TextField(
+                obscureText: _passwordVisible,
+                enableSuggestions: false,
+                autocorrect: false,
+                style: TextStyle(color: textFieldActiveColor),
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderSide:
+                        BorderSide(color: textFieldInactiveColor, width: 1),
+                    borderRadius:
+                        BorderRadius.all(Radius.circular(borderRadius)),
+                  ),
+                  hintStyle: TextStyle(color: textFieldInactiveColor),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide:
+                        BorderSide(color: textFieldActiveColor, width: 1),
+                    borderRadius:
+                        BorderRadius.all(Radius.circular(borderRadius)),
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius:
+                        BorderRadius.all(Radius.circular(borderRadius)),
+                  ),
+                  hintText: "Password",
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _passwordVisible
+                          ? Icons.visibility
+                          : Icons.visibility_off,
+                      color: Colors.grey,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _passwordVisible = !_passwordVisible;
+                      });
+                    },
+                  ),
+                ),
+                onChanged: (text) {
+                  password = text;
+                },
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 25,
+          ),
           SizedBox(
             width: 300,
             child: ElevatedButton(
-              onPressed: () {},
+              onPressed: () {
+                debugPrint("username:$username, email:$email, password:$password");
+              },
               style: ButtonStyle(
                 padding: MaterialStateProperty.all(EdgeInsets.all(0)),
-                // foregroundColor: MaterialStateProperty.all(Colors.white),
-                // overlayColor: MaterialStateProperty.all(
-                //     Color.fromARGB(71, 217, 217, 217)),
                 shape: MaterialStateProperty.all(
                   RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20),
+                    borderRadius: BorderRadius.circular(borderRadius),
                   ),
                 ),
                 backgroundColor: MaterialStateProperty.all(Colors.transparent),
               ),
               child: Ink(
                 width: 320,
-                decoration: const BoxDecoration(
+                decoration: BoxDecoration(
                   gradient: LinearGradient(
                     colors: [
                       Color(0xffF476FF),
@@ -94,14 +232,14 @@ class _AuthState extends State<Auth> {
                     begin: Alignment.topLeft,
                     end: Alignment.bottomCenter,
                   ),
-                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                  borderRadius: BorderRadius.all(Radius.circular(borderRadius)),
                 ),
                 child: Container(
                   child: Row(
                     mainAxisSize: MainAxisSize.min,
                     children: [
                       SizedBox(width: 120),
-                      Text("Log In",
+                      Text(widget.type == "login" ? "Log In" : "Sign Up",
                           style: const TextStyle(
                             fontSize: 18,
                             fontWeight: FontWeight.w600,
